9e3e516a2ff47257dddb83d8b77cf134efe957fd
Csonti <csontostamas1991@gmail.com>
2018-04-04 11:08:18 +0200

    restart button

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -1,8 +1,13 @@
 package com.codecool.snake;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class Main extends Application {
 
@@ -10,9 +15,41 @@ public class Main extends Application {
         launch(args);
     }
 
+
+    void cleanup() {
+        // stop animations reset model ect.
+    }
+
+
+    public void restart(Stage stage) {
+        cleanup();
+        start(stage);
+    }
+
+
+    void restartPopup(String infoMessage, String titleBar, Stage stage) {
+        int dialogButton = JOptionPane.YES_NO_OPTION;
+        int dialogResult = JOptionPane.showConfirmDialog(null, "Would You Like to restart the game?", "Restart!", dialogButton);
+        if (dialogResult == JOptionPane.YES_OPTION) {
+            restart(stage);
+        }
+
+    }
+
+
     @Override
     public void start(Stage primaryStage) {
+        Button button = new Button("Restart");
         Game game = new Game();
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                restartPopup("GG", "GG", primaryStage);
+            }
+        });
+
+        game.getChildren().add(button);
 
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
