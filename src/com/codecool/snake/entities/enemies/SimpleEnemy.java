64a5fc08ba7926c62bd09ee8b019dc6888b693e1
Oláh Tamás <tolah94@gmail.com>
2018-04-04 11:04:10 +0200

    Enemies bouncing off the wall

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -16,6 +16,7 @@ public class SimpleEnemy extends GameEntity implements Animatable, Interactable 
 
     private Point2D heading;
     private static final int damage = 10;
+    double direction;
 
     public SimpleEnemy(Pane pane) {
         super(pane);
@@ -27,7 +28,7 @@ public class SimpleEnemy extends GameEntity implements Animatable, Interactable 
         setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
         setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
 
-        double direction = rnd.nextDouble() * 360;
+        direction = rnd.nextDouble() * 360;
         setRotate(direction);
         heading = Utils.directionToVector(direction, speed);
     }
@@ -35,7 +36,60 @@ public class SimpleEnemy extends GameEntity implements Animatable, Interactable 
     @Override
     public void step() {
         if (isOutOfBounds()) {
-            destroy();
+            int wallDirection = whichWallTouched();
+            //up
+            if (wallDirection == 0) {
+                //left-to-right
+                if (direction >= 0 && direction < 90) {
+                    heading = Utils.directionToVector(180 - direction, 1);
+                    direction = 180 - direction;
+                }
+                //right-to-left
+                if (direction > 270) {
+                    heading = Utils.directionToVector(360 - direction + 180, 1);
+                    direction = 360 - direction + 180;
+                }
+            }
+            //right
+            if (wallDirection == 90) {
+                //down-to-up
+                if (direction > 0 && direction < 90) {
+                    heading = Utils.directionToVector(360 - direction, 1);
+                    direction = 360 - direction;
+                }
+                //up-to-down
+                if (direction >= 90 && direction < 180) {
+                    heading = Utils.directionToVector(360 - direction, 1);
+                    direction = 360 - direction;
+                }
+            }
+            //down
+            if (wallDirection == 180) {
+                //left-to-right
+                if (direction > 90 && direction <= 180) {
+                    heading = Utils.directionToVector(180 - direction, 1);
+                    direction = 180 - direction;
+                }
+                //right-to-left
+                if (direction > 180 && direction < 270) {
+                    heading = Utils.directionToVector(360 - direction + 180, 1);
+                    direction = 360 - direction + 180;
+                }
+            }
+            //left
+            if (wallDirection == 270) {
+                //down-to-up
+                if (direction > 270) {
+                    heading = Utils.directionToVector(360 - direction, 1);
+                    direction = 360 - direction;
+                }
+                //up-to-down
+                if (direction > 180 && direction <= 270) {
+                    heading = Utils.directionToVector(360 - direction, 1);
+                    direction = 360 - direction;
+                }
+            }
+
         }
         setX(getX() + heading.getX());
         setY(getY() + heading.getY());
